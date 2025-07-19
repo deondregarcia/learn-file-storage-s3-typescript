5e80e0398ef57d6be8484e4df91ff5bd8c57908b
deondregarcia <deondre.garcia@yahoo.com>
2025-07-19 14:30:56 -0700

    pre-caching lesson

--- src/api/assets.ts
+++ src/api/assets.ts
@@ -1,4 +1,5 @@
 import { existsSync, mkdirSync } from "fs";
+import path from "path";
 
 import type { ApiConfig } from "../config";
 
@@ -7,3 +8,20 @@ export function ensureAssetsDir(cfg: ApiConfig) {
     mkdirSync(cfg.assetsRoot, { recursive: true });
   }
 }
+
+export const mediaTypeToExt = (mediaType: string) => {
+  const split = mediaType.split("/");
+  if (split.length !== 2) {
+    return ".bin";
+  }
+
+  return "." + split[1];
+};
+
+export const getAssetDiskPath = (cfg: ApiConfig, assetPath: string) => {
+  return path.join(cfg.assetsRoot, assetPath);
+};
+
+export const isUnsupportedType = (mediaType: string) => {
+  return mediaType !== "image/png" && mediaType !== "image/jpeg";
+};
--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,40 +1,11 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
-
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import path from "path";
+import { getAssetDiskPath, isUnsupportedType, mediaTypeToExt } from "./assets";
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -47,7 +18,36 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  // TODO: implement the upload here
+  const formData = await req.formData();
+  const file = formData.get("thumbnail");
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Image not found");
+  }
 
-  return respondWithJSON(200, null);
+  // if file.type is neither of them
+  // if (!(file.type === "image/png" || file.type === "image/jpeg"))
+  if (isUnsupportedType(file.type))
+    throw new BadRequestError("Unsupported file type");
+
+  const MAX_UPLOAD_SIZE = 10 << 20;
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("File size greater than 10MB");
+  }
+
+  const imgArrayBuffer = await file.arrayBuffer();
+  const videoMetaData = getVideo(cfg.db, videoId);
+  if (userID !== videoMetaData?.userID) {
+    throw new UserForbiddenError("Incorrect user");
+  }
+
+  const fileExtension = mediaTypeToExt(file.type);
+  const filename = `${videoId}.${fileExtension}`;
+
+  const assetDiskPath = getAssetDiskPath(cfg, filename);
+  Bun.write(assetDiskPath, imgArrayBuffer);
+  videoMetaData.thumbnailURL = `http://localhost:${cfg.port}/assets/${videoId}.${fileExtension}`;
+
+  updateVideo(cfg.db, videoMetaData);
+
+  return respondWithJSON(200, videoMetaData);
 }
